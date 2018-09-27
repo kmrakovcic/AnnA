cbd7be3ed7bfe6601c61985c8465ec00f413afd0
Nextgreen <mrakovcickarlo@gmail.com>
2018-09-27 18:32:12 +0200

    Added possibility for dividing to test and train datasets

--- SQL.py
+++ SQL.py
@@ -67,14 +67,27 @@ def edit_list(lista):
 	out=np.hstack((lista[:,:-1], result,lista[:,-1][:, np.newaxis]))
 	return out,[lista.shape[1]-1, result.shape[1]]
 
-def mainSQL():
+def divideList (lista):
+	np.random.shuffle(lista) #TRAIN pa TEST
+	return lista[:int(round(lista.shape[0]*0.7,0)),:], lista[int(round(lista.shape[0]*0.7,0)):,:]
+
+def mainSQL(notdivided=True):
 	URL=input ("URL?: ")
+	if URL == "": URL="https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 	name=input ("Table name?: ")
-	db=input ("Database?: ")
+	if name == "": name="Iris"
+	db=input ("Database name?: ")
+	if db == "": db="Datasets"
 	a= edit_list(url_to_list(URL))
-	list_to_sql (a[0],a[1],name,db+".db")
-	print ("Table "+name+" created in database "+db)
+	if notdivided:
+		b=divideList (a[0])
+		list_to_sql (b[0],a[1],name+"_train",db+".db")
+		list_to_sql (b[1],a[1],name+"_test",db+".db")
+		print ("Table "+name+"_train and "+name+"_test created in database "+db)
+	else:
+		list_to_sql (a[0],a[1],name,db+".db")
+		print ("Table "+name+" created in database "+db)
 	input ("PRESS ENTER TO EXIT")
 
 if __name__ == '__main__':
-	mainSQL()
+	mainSQL(False)
