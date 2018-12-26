c17c61ff79a7215c18b40d91e482590fa4de04ad
Nextgreen <mrakovcickarlo@gmail.com>
2018-12-26 15:00:03 +0100

    Some comments

--- AnnA3.py
+++ AnnA3.py
@@ -1,6 +1,6 @@
 from AnnA3mathlib import *
 
-def resetParameters (arh):
+def resetParameters (arh):  #postavlja sve weightove i biase na 0
 	w=[0]
 	b=[0]
 	for i,j in enumerate (arh):
@@ -11,26 +11,26 @@ def resetParameters (arh):
 
 class Brain: 
 	def __init__ (self, arhitecture=[1,1], mjerenja=[], alpha=0.1, activationFunction=Activationfunction.sigmoid, errorFunction=Errorfunction.cost, fixedAlpha=True):
-		self.n=[]
-		self.b=[]
-		self.w=[]
-		self.newb=[]
-		self.neww=[]
-		self.z=[]
-		self.delta=[]
-		self.alpha=alpha
-		self.mjerenja=mjerenja
-		self.activationfunction=activationFunction
-		self.errorFunction=errorFunction
+		self.n=[]           	#neuroni
+		self.b=[]				#biasi
+		self.w=[]				#weightovi
+		self.newb=[]			#pomoćni biasi
+		self.neww=[]			#pomoćni weightovi
+		self.z=[]				#neuroni bez aktivacijske funkcije
+		self.delta=[]			#delte
+		self.alpha=alpha 		#learning rate
+		self.mjerenja=mjerenja  #izmjereni podatci
+		self.activationfunction=activationFunction  #aktivacijska funkcija
+		self.errorFunction=errorFunction 			#error funkcija
 		if not mjerenja==[]:
 			arhitecture[0]=mjerenja[0].shape [0]
 			arhitecture[len(arhitecture)-1]=mjerenja[1].shape[0]
-		self.arhitecture=np.array(arhitecture)
-		self.fixedAlpha = fixedAlpha
+		self.arhitecture=np.array(arhitecture)      #arhitektura mozga
+		self.fixedAlpha = fixedAlpha                #mjenjanje learning rate-a?
 
-	def birth (self):
+	def birth (self):    #stvara tenzore alfi, neurona, weightova i biasa, te stavlja u njih random vrijednosti
 		if not self.mjerenja==[]:
-			M=self.mjerenja[1].shape[1]
+			M=self.mjerenja[1].shape[1]  #gleda tenzor mjerenja i iz njega gleda koliko mjerenja ima
 		else: M=1
 		self.b=[0]
 		self.w=[0]
@@ -38,30 +38,30 @@ class Brain:
 		self.neww=[0]
 		self.delta=[0]
 		for i,j in enumerate (self.arhitecture):
-			self.n.append (np.ones((j,M)) )
-			self.z.append (np.ones ((j,M))) 
+			self.n.append (np.ones((j,M)) )     #radi jediničnu matricu neurona (arhitektura mozga x broj mjerenja) za svaku liniju neurona
+			self.z.append (np.ones ((j,M))) 	#isto to za z
 			if not (i==0):
-				self.b.append (np.random.random (j) )
-				self.w.append (np.random.random ((j, self.arhitecture [i-1])))
-				self.delta.append ([])
-				self.neww.append ([])
+				self.b.append (np.random.random (j) )  #radi vektor biasa (duljine koliko kaže arhitektura mozga) za svaki red neurona
+				self.w.append (np.random.random ((j, self.arhitecture [i-1]))) #radi tenzor weightova za svaki red neurona, s tim da je 0. red neurona prazan i weigtovi su od pozicije 2
+				self.delta.append ([]) #prazni vektori za svaki red neurona za alfe i pomoćne neurone i weightove
+				self.neww.append ([]) 
 				self.newb.append ([])
 
 	def fowardpropagation (self, new=False):
-		self.n[0]=self.mjerenja[0]
-		if new:
+		self.n[0]=self.mjerenja[0] #na input red neurona stavlja izmjerene vrijednosti
+		if new:                    #gleda čisto dal radi foward propagaciju na pomoćnim neuronima ili na pravim
 			for i in range(len(self.n)-1):
 				self.z[i+1]=self.neww[i+1].dot(self.n[i])+self.newb[i+1].reshape(-1,1)
 				self.n[i+1]=self.activationfunction (self.z[i+1])
 		else:
 			for i in range(len(self.n)-1):
-				self.z[i+1]=self.w[i+1].dot(self.n[i])+self.b[i+1].reshape(-1,1)
-				self.n[i+1]=self.activationfunction (self.z[i+1])
+				self.z[i+1]=self.w[i+1].dot(self.n[i])+self.b[i+1].reshape(-1,1)   #sumiranje prijašnijh neurona u z (neuron bez aktivacijske funkcije)
+				self.n[i+1]=self.activationfunction (self.z[i+1])                  #djelovanje aktivacijske funkcije u smjeru z-a, i to je onda neuron :D
 
 	def backpropagation (self):
 		y=self.mjerenja[1]
-		for l in reversed(range(1,len(self.n))):
-			if l==(len(self.n)-1):
+		for l in reversed(range(1,len(self.n))):  #ide od zadnjeg reda neurona prema prvom
+			if l==(len(self.n)-1):                #algoritam za backpropagaciju
 				self.delta[l]=self.n[l]-y
 			else:
 				self.delta[l]=self.w[l+1].T.dot(self.delta[l+1])*self.activationfunction (self.z[l], d=True)
